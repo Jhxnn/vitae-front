5478d66ee9d3fb6558088eb4ec64e79410dc301a
Hilixx <jhonalizan@gmail.com>
2025-07-28 10:53:32 -0300

    tela de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,31 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) },
-  { path: 'register', loadChildren: () => import('./features/register/register.module').then(m => m.RegisterModule) },
-  { path: 'upload-cv', loadChildren: () => import('./features/upload-cv/upload-cv.module').then(m => m.UploadCvModule) },
-  { path: 'ranking', loadChildren: () => import('./features/ranking/ranking.module').then(m => m.RankingModule) },
-  { path: 'user-cvs', loadChildren: () => import('./features/user-cvs/user-cvs.module').then(m => m.UserCvsModule) },
-
-  // Redirecionamento padrão
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-
-  // Página 404
-  { path: '**', redirectTo: 'login' }
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./features/register/register.module').then(m => m.RegisterModule)
+  },
+  {
+    path: 'upload-cv',
+    loadChildren: () => import('./features/upload-cv/upload-cv.module').then(m => m.UploadCvModule)
+  },
+  {
+    path: 'user-cvs',
+    loadChildren: () => import('./features/user-cvs/user-cvs.module').then(m => m.UserCvsModule)
+  },
+  {
+    path: 'ranking',
+    loadChildren: () => import('./features/ranking/ranking.module').then(m => m.RankingModule)
+  }
 ];
 
 @NgModule({
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        hero: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
